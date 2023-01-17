22af3b93224eb47574f4fa0673b4c543cc80ab33
OlgaDzhumanova <dzhum-olga@yandex.ru>
2023-01-17 10:07:06 +0300

    Что такое обобщенные типы (generics) [#4952 #380461]

--- src/main/java/ru/generics/Generics.java
+++ src/main/java/ru/generics/Generics.java
@@ -1,7 +1,6 @@
 package ru.generics;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,14 +19,14 @@ public class Generics {
         gen.printObject(third);
         System.out.println();
 
-        gen.printBoundedWildCard((List<? extends Predator>) first);
+        gen.printBoundedWildCard(first);
         gen.printBoundedWildCard(second);
         gen.printBoundedWildCard(third);
         System.out.println();
 
         gen.printLowerBoundedWildCard(first);
         gen.printLowerBoundedWildCard(second);
-        gen.printLowerBoundedWildCard(Collections.singletonList(third));
+        gen.printLowerBoundedWildCard(third);
     }
 
     public void printObject(List<?> list) {
@@ -37,15 +36,15 @@ public class Generics {
         }
     }
 
-    public void printBoundedWildCard(List<? extends Predator> list) {
-        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
+    public void printBoundedWildCard(List<? extends Animal> list) {
+        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
     }
 
-    public void printLowerBoundedWildCard(List<? super Predator> list) {
-        for (Iterator<? super Predator> it = list.iterator(); it.hasNext();) {
+    public void printLowerBoundedWildCard(List<? super Tiger> list) {
+        for (Iterator<? super Tiger> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
