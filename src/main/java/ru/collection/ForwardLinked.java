b008ca457130b7de95c3b62bd2296e07a9ed4bab
OlgaDzhumanova <dzhum-olga@yandex.ru>
2023-01-26 15:02:05 +0300

    Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/collection/ForwardLinked.java
@@ -0,0 +1,85 @@
+package ru.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            size++;
+            modCount++;
+            return;
+        }
+        Node<T> last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = node;
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> rsl = head;
+        for (var i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return rsl.item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        } else {
+             head = head.next;
+        }
+        return head.item;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private Node<T> lastElement = head;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return lastElement != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T result = lastElement.item;
+                lastElement = lastElement.next;
+                return result;
+            }
+
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
