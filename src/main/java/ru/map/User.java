4fd69f2baf96237d814b6a72beb46bf11cffc28a
OlgaDzhumanova <dzhum-olga@yandex.ru>
2023-02-21 15:55:37 +0300

    Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/map/User.java
+++ src/main/java/ru/map/User.java
@@ -1,6 +1,8 @@
 package ru.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +13,29 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+        User userOne = new User("Maya", 3, birthday);
+        map.put(userOne, new Object());
+        int hashCodeOne = userOne.hashCode();
+        int hashOne = hashCodeOne ^ (hashCodeOne >>> 16);
+        int backetOne = hashOne & 15;
+        User userTwo = new User("Maya", 3, birthday);
+        map.put(userTwo, new Object());
+        int hashCodeTwo = userTwo.hashCode();
+        int hashTwo = hashCodeTwo ^ (hashCodeTwo >>> 16);
+        int backetTwo = hashTwo & 15;
+
+        System.out.printf("userOne = хешкод: %d , хеш: %d , бакет: %d",
+                hashCodeOne, hashOne, backetOne);
+
+        System.out.println();
+
+        System.out.printf("userTwo = хешкод: %d , хеш: %d , бакет: %d",
+                hashCodeTwo, hashTwo, backetTwo);
+
     }
 }
