a746865de463e23e5c674a9827ddecabca66ff13
OlgaDzhumanova <dzhum-olga@yandex.ru>
2023-02-28 11:29:17 +0300

    Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/map/User.java
+++ src/main/java/ru/map/User.java
@@ -3,6 +3,7 @@ package ru.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -36,6 +37,23 @@ public class User {
 
         System.out.printf("userTwo = хешкод: %d , хеш: %d , бакет: %d",
                 hashCodeTwo, hashTwo, backetTwo);
+    }
 
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
